abe55e146e69a416b080bc531a132d8ad719eb2b
Shunming1994 <angusshuntsai@gmail.com>
2019-01-28 20:22:09 -0800

    jobform

--- myapp/src/views/LoginPage.vue
+++ myapp/src/views/LoginPage.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="LoginPage">
-	  <div class="wrapper">
-		<div v-if="page===1">
-			<MyLogin/>
-		</div>
-		<div v-if="page===2">
-			<MyReg/>
-		</div>
-	</div>
+	  <p>Hello</p>
 </div>
 </template>
 
@@ -17,22 +10,16 @@ import HelloWorld from '@/components/HelloWorld.vue';
 import Login from "@/components/Login.vue";
 import Register from "@/components/Register.vue";
 	export default {
+		name:"LoginPage",
 		components: {
 			MyLogin:Login,
 			MyReg:Register
 		},
 		data(){
 			return {
-				page:1
 			}
 		},
 		methods: {
-			changeLogin:function(){
-				this.page=1;
-			},
-			changeReg:function(){
-				this.page=2;
-			}
 		}
 	}
 </script>
@@ -41,31 +28,7 @@ import Register from "@/components/Register.vue";
 @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css");
 </style>
 <style>
-	.submitWrap {
-		text-align: center;
-	}
-	#submitButt {
-		margin: 4px;
-	}
-	.LoginPage {
-		display:flex;
-		position:fixed;
-		left:0;
-		top:0;
-		width:100vw;
-		height:100vh;
-		justify-content:center;
-		align-items:center;
-		text-align: left;
-		color: #000;
-		z-index: -1;
-		background: linear-gradient(-135deg, #c850c0, #4158d0);
-	}
-	.formContainer {
-		background-color: white;
-		padding: 30px 80px;
-		border-radius: 15px;
-	}
+
 </style>
 
 
--- myapp/src/views/resumeUpload.vue
+++ myapp/src/views/resumeUpload.vue
@@ -1,35 +1,30 @@
 <template>
-  <div class="resumeUpload">
-	  <clientHeader />
-	  <div class="formWrapper">
-      <div class="formcontainer">
-			<h2 class="resumeH">Resume and Profile</h2>
-		  <input type="text" class="form-control" v-model="name"
-			   id="exampleInputName" placeholder="Preferred name">
-		  <input type="email" class="form-control" v-model="email"
-			   id="exampleInputEmail1" aria-describedby="emailHelp" 
-			   placeholder="Enter email">
-		  <input type="text" class="form-control" v-model="number"
-				   id="exampleInputNumber" placeholder="Phone Number">
-		<div class="submitWrap">
-			<div class="row">
-  				<div class="col-6">
-	  				<p class="uploadR" >Upload Resume</p>
-		  			<button v-on:click="signup" type="submit" class="btn btn-primary uploadBtn">Choose File</button></div>
-  				<div class="col-6">
-					<button v-on:click="signup" type="submit" class="btn btn-primary updateBtn">Update Profile</button>
-				</div>
-			</div>
-		</div>
-      </div>
-	</div>
+<div class="resumeUpload">
+	<clientHeader />
+	<div class="formWrapper">
+		<form class="formContainer">
+		  <div class="form-group">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+		  </div>
+		  <div class="form-group">
+			<label for="exampleInputPassword1">Password</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		  </div>
+		  <div class="form-group">
+			<label for="exampleInputFile">File input</label>
+			<input type="file" id="exampleInputFile">
+		  </div>
+
+		  <button type="submit" class="btn btn-default">Submit</button>
+		</form>
+</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-import Login from '@/components/Login.vue';
 import clientHeader from '@/components/clientHeader.vue';
 import firebase from 'firebase';
 	
@@ -42,7 +37,6 @@ export default {
 	methods:{
 	},
 	components: {
-		Login:Login,
 		clientHeader:clientHeader,
 	}
 }
@@ -52,50 +46,26 @@ export default {
 @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css");
 </style>
 <style>
-	.resumeH {
-		float: left;
-		margin:20px;
-	}
-	.form-control {
-		margin: 20px;
-	}
-	.updateBtn {
-		float: right;
-	}
-	.uploadBtn {
-		text-align: left;
-		float: left;
-	}
-	.uploadR {
-		text-align:left;
-		padding: 0;
-		margin:0;
-	}
-	.submitWrap {
-		text-align: center;
-		width: 100%;
-		
+	.resumeUpload {
+		width: 100vw;
+		height: 100vh;
+		background-color: blanchedalmond;
 	}
 	.formWrapper {
+		background-color: aquamarine;
 		display:flex;
 		position:fixed;
-		left:0;
-		top:0;
 		width:100vw;
 		height:100vh;
 		justify-content:center;
 		align-items:center;
 		text-align: left;
 		color: #000;
-		z-index: -1;
+		background: linear-gradient(-135deg, #c850c0, #4158d0);
 	}
-	.formcontainer {
-		justify-content:center;
-		align-items:center;
-		padding: 30px 80px;
-		border-radius: 15px;
-		width: 30%;
+	.formContainer {
+		text-align: left;
+		padding: 100px;
 		
 	}
-
 </style>
