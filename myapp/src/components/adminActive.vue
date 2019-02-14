6d4b6843027ecb586ba124d0b2db094338d5582b
carmen-c <carmen.csmwc@gmail.com>
2019-02-13 18:37:22 -0800

    move edit into post

--- myapp/src/components/adminActive.vue
+++ myapp/src/components/adminActive.vue
@@ -1,41 +1,53 @@
 <template>
   <div class="adminActive">
-    <div class="edit">
-      <editPost
-        v-if="editPostDisplay == true"
-        v-bind:post_id="post_id"
-        v-bind:position="position"
-        v-bind:company="company"
-        v-bind:location="location"
-        v-bind:description="description"
-        v-bind:start="start.seconds"
-        v-bind:end="end.seconds"
-      />
-    </div>
-    <div class="container activePost">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="text-right pull-right">
-            <button class="admin-posting-btn" @click="editPost">Edit</button>
-            <button class="admin-posting-btn" @click="deletePost">Delete</button>
+    <b-card no-body class="mb-1" id="cardWrapper">
+      <div class="container activePost">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="text-right pull-right">
+              <button
+                block
+                href="#"
+                @click="showCollapse = !showCollapse"
+                class="admin-posting-btn"
+                :class="showCollapse ? 'collapsed':'null'"
+                :aria-controls="post_id"
+                :aria-expanded="showCollapse ? 'true':'false'"
+                type="submit"
+              >Edit</button>
+              <button class="admin-posting-btn" @click="deletePost">Delete</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col-md-2">
-          <p>Logo Here</p>
+        <div class="row">
+          <div class="col-md-2">
+            <p>Logo Here</p>
+          </div>
+          <div class="col-md-10 text-left">
+            <h1>{{position}}</h1>
+            <p>{{company}} - {{location}}</p>
+            <p>
+              <img class="contractIcon jobDuration" src="../../images/contract.svg">
+              {{start.seconds | moment("MMMM Do YYYY")}} to {{end.seconds | moment("MMMM Do YYYY")}}
+            </p>
+          </div>
         </div>
-        <div class="col-md-10 text-left">
-          <h1>{{position}}</h1>
-          <p>{{company}} - {{location}}</p>
-          <p>
-            <img class="contractIcon jobDuration" src="../../images/contract.svg">
-            {{start.seconds | moment("MMMM Do YYYY")}} to {{end.seconds | moment("MMMM Do YYYY")}}
-          </p>
-        </div>
+        <b-collapse :id="post_id" role="tabpanel" v-model="showCollapse">
+          <b-card-body>
+            <editPost
+              v-bind:post_id="post_id"
+              v-bind:position="position"
+              v-bind:company="company"
+              v-bind:location="location"
+              v-bind:description="description"
+              v-bind:start="start.seconds"
+              v-bind:end="end.seconds"
+            />
+          </b-card-body>
+        </b-collapse>
       </div>
-    </div>
+    </b-card>
   </div>
 </template>
 
@@ -50,7 +62,7 @@ export default {
   },
   data() {
     return {
-      editPostDisplay: false
+      showCollapse: false
     };
   },
   methods: {
--- myapp/src/views/jobFeedPage.vue
+++ myapp/src/views/jobFeedPage.vue
@@ -1,94 +1,117 @@
 <template>
- <div class="jobFeedPage">
-	 <clientheader/>
-	 <div class="row">
-<!--******* LEFT SIDE OF THE PAGE *************		 -->
-	  <div class="col-md-3 marginTop linkStyles">
-		<div class="recent-Location">
-		<h4 class="h4MarginBottom">Recent Searches:</h4>
-		  <p><a href="#">Graphic Designer</a></p>
-		  <p><a href="#">Developer</a></p>
-		  <p><a href="#">Content Creator</a></p>
-		  <p><a href="#">UI/UX Design</a></p>
-		  <p><a href="#">Marketing</a></p>
-	</div>
-<hr class="line"/>
-	<div class="recent-Location">
-		<h4 class="h4MarginBottom">Locations:</h4>
-		  <p><a href="#">Vancouver (104)</a></p>
-		  <p><a href="#">Burnaby (92)</a></p>
-		  <p><a href="#">Richmond (59)</a></p>
-		  <p><a href="#">Coquitlam (36)</a></p>
-		<p><a href="#">Surrey (6)</a></p>
-	</div>
-<hr class="line"/>
-	<div class="recent-Location">
-		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Clear All Searches</button>
-	</div>
-</div>
-		 
-<!--<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>-->
-<!--******* RIGHT SIDE OF THE PAGE *************		 -->
-	 <div class="col-md-9">
-		 <div class="test1">
-		<div class="row">
-			<!--DROP DOWN MENU -->
-			  <div class="col-xs-12 col-md-12 dropDown">
-				  <div>
-					<b-form inline>
-					  <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" placeholder="Internship Title" />
-					  <b-input-group left="@" class="mb-2 mr-sm-2 mb-sm-0">
-						<b-input id="inlineFormInputGroupUsername2" placeholder="Location" />
-					  </b-input-group>
-						<div>
-						  <b-dropdown id="ddown1" text="Sort By" class="m-md-2">
-							<b-dropdown-item>Most Recent</b-dropdown-item>
-							<b-dropdown-item>Full time</b-dropdown-item>
-							<b-dropdown-item>Part time</b-dropdown-item>
-						  </b-dropdown>
-						</div>
-					</b-form>
-				  </div>
-			</div>
-		</div>
-		 </div>
-		<!--	THIS SECTION CREATE TEMPLATE THAT CAN REPEAT	  -->
-          <jobPostContainer 
-            v-for="job in jobsArray"
-            v-bind:post_id="job.post_id"
-            v-bind:position="job.position"
-            v-bind:company="job.company"
-            v-bind:location="job.location"
-            v-bind:description="job.description"
-            v-bind:start="job.start_date"
-            v-bind:end="job.end_date"
-          />
+  <div class="jobFeedPage">
+    <clientheader/>
+    <div class="row">
+      <!--******* LEFT SIDE OF THE PAGE *************		 -->
+      <div class="col-md-3 marginTop linkStyles">
+        <div class="recent-Location">
+          <h4 class="h4MarginBottom">Recent Searches:</h4>
+          <p>
+            <a href="#">Graphic Designer</a>
+          </p>
+          <p>
+            <a href="#">Developer</a>
+          </p>
+          <p>
+            <a href="#">Content Creator</a>
+          </p>
+          <p>
+            <a href="#">UI/UX Design</a>
+          </p>
+          <p>
+            <a href="#">Marketing</a>
+          </p>
+        </div>
+        <hr class="line">
+        <div class="recent-Location">
+          <h4 class="h4MarginBottom">Locations:</h4>
+          <p>
+            <a href="#">Vancouver (104)</a>
+          </p>
+          <p>
+            <a href="#">Burnaby (92)</a>
+          </p>
+          <p>
+            <a href="#">Richmond (59)</a>
+          </p>
+          <p>
+            <a href="#">Coquitlam (36)</a>
+          </p>
+          <p>
+            <a href="#">Surrey (6)</a>
+          </p>
+        </div>
+        <hr class="line">
+        <div class="recent-Location">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Clear All Searches</button>
+        </div>
+      </div>
+
+      <!--<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>-->
+      <!--******* RIGHT SIDE OF THE PAGE *************		 -->
+      <div class="col-md-9">
+        <div class="test1">
+          <div class="row">
+            <!--DROP DOWN MENU -->
+            <div class="col-xs-12 col-md-12 dropDown">
+              <div>
+                <b-form inline>
+                  <b-input
+                    class="mb-2 mr-sm-2 mb-sm-0"
+                    id="inlineFormInputName2"
+                    placeholder="Internship Title"
+                  />
+                  <b-input-group left="@" class="mb-2 mr-sm-2 mb-sm-0">
+                    <b-input id="inlineFormInputGroupUsername2" placeholder="Location"/>
+                  </b-input-group>
+                  <div>
+                    <b-dropdown id="ddown1" text="Sort By" class="m-md-2">
+                      <b-dropdown-item>Most Recent</b-dropdown-item>
+                      <b-dropdown-item>Full time</b-dropdown-item>
+                      <b-dropdown-item>Part time</b-dropdown-item>
+                    </b-dropdown>
+                  </div>
+                </b-form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--	THIS SECTION CREATE TEMPLATE THAT CAN REPEAT	  -->
+        <jobPostContainer
+          v-for="job in jobsArray"
+          v-bind:post_id="job.post_id"
+          v-bind:position="job.position"
+          v-bind:company="job.company"
+          v-bind:location="job.location"
+          v-bind:description="job.description"
+          v-bind:start="job.start_date"
+          v-bind:end="job.end_date"
+        />
         <!--	PUT THE ACCORDION HERE	  -->
-       
-	</div> 
-</div>
-</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import clientheader from '@/components/clientHeader.vue'
-import firebase from 'firebase';
-import jobPostContainer from '@/components/jobPostContainer.vue'
+import clientheader from "@/components/clientHeader.vue";
+import firebase from "firebase";
+import jobPostContainer from "@/components/jobPostContainer.vue";
 // Boot Strap Vue
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-  
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 export default {
-  name:"LoginPage",
+  name: "LoginPage",
   components: {
-      clientheader,
-      jobPostContainer
+    clientheader,
+    jobPostContainer
   },
-  data(){
-      return {
-        jobsArray: []
-      }
+  data() {
+    return {
+      jobsArray: []
+    };
   },
   created: function() {
     this.getJobs();
@@ -97,82 +120,81 @@ export default {
     getJobs: function() {
       var jobs = firebase.firestore().collection("jobs");
       jobs.get().then(
-        (snapshot) => {
-            snapshot.forEach((doc)=>{
-              var data = doc.data();
-                 this.jobsArray.push({
-                   post_id: data.post_id,
-                   uid: data.uid,
-                   position: data.position,
-                   company: data.company,
-                   location: data.location,
-                   start_date: data.start_date,
-                   end_date: data.end_date,
-                   description: data.description
-                 })
+        snapshot => {
+          snapshot.forEach(doc => {
+            var data = doc.data();
+            this.jobsArray.push({
+              post_id: data.post_id,
+              uid: data.uid,
+              position: data.position,
+              company: data.company,
+              location: data.location,
+              start_date: data.start_date,
+              end_date: data.end_date,
+              description: data.description
             });
+          });
         },
-        (err) => {
-            alert(err.message)
+        err => {
+          alert(err.message);
         }
       );
-    }   
+    }
   }
-}
-
+};
 </script>
 
 <style>
-@import url("//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css")
+@import url("//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css");
 @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css");
-	.col-md-3 {
-	}
-	.col-md-9 {
-	}
+
+.col-md-3 {
+}
+.col-md-9 {
+}
 /*	*******************************  */
-	.line {
-		margin-top: 20px;
-        margin-left: 70px;
-		width: 50%;
-		border:.5px solid #7fd686;
-	}
+.line {
+  margin-top: 20px;
+  margin-left: 70px;
+  width: 50%;
+  border: 0.5px solid #7fd686;
+}
 /*	*******************************  */
 /*	Left Side  */
-	.recent-Location {
-		text-align: left;
-		padding-left: 70px;
-	}
-	.marginTop {
-		margin-top: 50px;
-	}
-	.h4MarginBottom {
-		margin-bottom: 20px;
-	}
-	.linkStyles a {
-		color: black;
-	}
-	.linkStyles {
-		
-	}
+.recent-Location {
+  text-align: left;
+  padding-left: 70px;
+}
+.marginTop {
+  margin-top: 50px;
+}
+.h4MarginBottom {
+  margin-bottom: 20px;
+}
+.linkStyles a {
+  color: black;
+}
+.linkStyles {
+}
 /*	*******************************  */
 /*	Right Side  */
-	.dropDown {
-		padding: 20px;
-	}
-	.inputWidth {
-		width: 200px;
-	}
-	.test1 {
-		justify-content: left;
-		display: flex;
-		padding: 20px;
-	}
-	.jobFeedPage {
-		overflow: hidden;
-	}
-	.test1 {
-		margin-left: 30px;
-	}
+.dropDown {
+  padding: 20px;
+}
+.inputWidth {
+  width: 200px;
+}
+.test1 {
+  justify-content: left;
+  display: flex;
+  padding: 20px;
+}
+.jobFeedPage {
+  overflow: hidden;
+}
+.test1 {
+  margin-left: 30px;
+}
 </style>
 
 
