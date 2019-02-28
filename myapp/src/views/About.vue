17fed2ba835967e6634189a9654fae74cfb2c5ac
carmen-c <carmen.csmwc@gmail.com>
2019-02-28 11:40:58 -0800

    about component

---
+++ myapp/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="About">
+    <h1>HERE IS THE ABOUT SECTION</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "about"
+};
+</script>
+<style>
+.About {
+  background-color: aqua;
+  margin-top: 500px;
+}
+</style>
